0c38e96812ef02298bf9241e4a016c6c00b0c1c7
scv1990 <fuqinhq@163.com>
2016-08-18 18:11:01 +0800

    98

--- src/com/yisa/pray/activity/PerfectUserinfoActivity.java
+++ src/com/yisa/pray/activity/PerfectUserinfoActivity.java
@@ -8,8 +8,12 @@
 
 package com.yisa.pray.activity;
 
+import android.view.View;
+import android.widget.EditText;
+
 import com.lidroid.xutils.ui.BaseActivity;
 import com.yisa.pray.R;
+import com.yisa.pray.views.CustomHeadView;
 
 /**
  *
@@ -23,7 +27,19 @@ import com.yisa.pray.R;
  * 修改备注:
  */
 public class PerfectUserinfoActivity extends BaseActivity {
-
+	private CustomHeadView mHeadView;
+	private EditText mUserName;
+	private EditText mAge;
+	private EditText mTel;
+	private EditText mEducation;
+	private EditText mVocation;
+	private EditText mChurch;
+	private EditText mChurchService;
+	private EditText mArea;
+	private EditText mPeriod;
+	private EditText mRebirth;
+	private EditText mAddress;
+	
 	@Override
 	public void setRootLayout() {
 		setContentView(R.layout.activity_perfected_userinfo);
@@ -31,7 +47,27 @@ public class PerfectUserinfoActivity extends BaseActivity {
 
 	@Override
 	public void initView() {
-		
+		mHeadView = (CustomHeadView) getView(R.id.head_view);
+		mHeadView.setLeftIconClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		mUserName = (EditText) getView(R.id.name);
+		mAge = (EditText) getView(R.id.tel);
+		mTel = (EditText) getView(R.id.age);
+		mEducation = (EditText) getView(R.id.education);
+		mVocation = (EditText) getView(R.id.vocation);
+		mChurch = (EditText) getView(R.id.church);
+		mChurchService = (EditText) getView(R.id.church_service);
+		mArea = (EditText) getView(R.id.pray_area);
+		mPeriod = (EditText) getView(R.id.pray_period);
+		mRebirth = (EditText) getView(R.id.rebirth);
+		mAddress = (EditText) getView(R.id.address);
 	}
 
+	public void commitData(){
+		
+	}
 }
